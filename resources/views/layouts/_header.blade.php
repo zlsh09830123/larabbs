32ffe26359a09cdd8c1d1dd6e7878e90c1de6590
zlsh09830123 <zlsh09830123@gmail.com>
2021-02-12 15:22:47 +0800

    修復跳轉連結

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -17,8 +17,28 @@
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登入</a></li>
         <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">註冊</a></li>
+        @else
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="#" class="dropdown-item">個人中心</a>
+            <a href="#" class="dropdown-item">編輯資料</a>
+            <div class="dropdown-divider"></div>
+            <a href="#" class="dropdown-item" id="logout">
+              <form action="{{ route('logout') }}" method="post">
+                @csrf
+                <button class="btn btn-block btn-danger" type="submit" name="button">登出</button>
+              </form>
+            </a>
+          </div>
+        </li>
+        @endguest
       </ul>
     </div>
   </div>
