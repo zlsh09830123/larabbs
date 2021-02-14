7d360136a40b8052859a373ee103d35408755401
zlsh09830123 <zlsh09830123@gmail.com>
2021-02-14 17:14:55 +0800

    授權訪問

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -24,7 +24,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        // 修改策略自動發現的邏輯
+        Gate::guessPolicyNamesUsing(function ($modelClass) {
+            // 動態返回模型對應的策略名稱，如： // 'App\Model\User' => 'App\Policies\UserPolicy',
+            return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
+        });
     }
 }
